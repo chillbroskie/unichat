47d9f99ddc730992970acd97350cce3378e1905e
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-10-08 10:38:48 -0400

    start of app
    started the chats component, login, and context and handlend user login so far

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const auth = firebase.initializeApp({
+  apiKey: "AIzaSyBnCS9vCKkLlxUB1W2ENfHuUGJ-gLYWseo",
+  authDomain: "unichat-f80da.firebaseapp.com",
+  projectId: "unichat-f80da",
+  storageBucket: "unichat-f80da.appspot.com",
+  messagingSenderId: "757302202095",
+  appId: "1:757302202095:web:791754344ed97ee7f2551a"
+}).auth();
